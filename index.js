a3e29340d011c2056511f21f5843ea41e19c7f6f
didericis <didericis@gmail.com>
2018-04-21 23:48:37 -0400

    Remove style-loader

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const airbnb = require('@neutrinojs/airbnb');
 const reactComponents = require('@neutrinojs/react-components');
 const mocha = require('@neutrinojs/mocha');
-const styleLoader = require('@neutrinojs/style-loader');
 
 module.exports = (neutrino) => {
   neutrino.use(airbnb);
   neutrino.use(mocha);
-  neutrino.use(reactComponents)
-  neutrino.use(styleLoader, { 
-    extractId: 'extract', 
-    extract: { plugin: { filename: '[name].css' } } 
+  neutrino.use(reactComponents, {
+    style: { extract: { plugin: { filename: '[name].css' } } }
   });
 };
